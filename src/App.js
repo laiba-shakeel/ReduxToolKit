9973f0a4c9a945bff2a96a60fe4cf933d89a97c9
laiba-shakeel <aqsa.shaqil@gmail.com>
2023-06-03 22:02:32 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import Counter from "./features/counter/Counter";
-
+// import Counter from "./features/counter/Counter";
+import PostList from "./features/post/PostList";
+import AddPostForm from "./features/post/AddPostForm";
 function App() {
   return (
    <main className='App'>
-<Counter />
+{/* <Counter /> */}
+<AddPostForm />
+<PostList/>
+
    </main>
   );
 }
---
+++ src/features/post/postSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [
+    {id: '1' , title: 'Learning Redux Tollkit' , content: "I`ve heard good think"},
+    { id:'2' , title: 'slice...' , content: "the more I say slice , the more I want pizza"}
+]
+
+const postSlice = createSlice({
+    name: 'post', 
+    initialState, 
+    reducers: {
+
+        postAdded(state , action) {
+            state.push(action.payload)
+        }
+    }
+})
+
+export const selectAllPosts = (state) => state.posts;
+
+export const {postAdded} = postSlice.actions
+
+export default postSlice.reducer
+
